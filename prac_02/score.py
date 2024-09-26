11460794cd84f20d50830b5a10d229195c3031f9
Mr-Hadez <hwest1507@gmail.com>
2024-09-27 09:30:48 +1000

    Refactor score status program with functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,23 @@ CP1404
 Amended a broken program to determine score status
 """
 
-score = float(input("Enter score: "))
 
-if 100 >= score >= 90:
-    print("Excellent")
-elif 90 > score >= 50:
-    print("Passable")
-elif 0 <= score < 50:
-    print("Bad")
-else:
-    print("Invalid score")
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score_status(score)
+    print(f"Your score is {result}")
+
+
+def determine_score_status(score):
+    if 100 >= score >= 90:
+        result = "Excellent"
+    elif 90 > score >= 50:
+        result = "Passable"
+    elif 0 <= score < 50:
+        result = "Bad"
+    else:
+        result = "Invalid score"
+    return result
+
+
+main()
