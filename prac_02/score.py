13b7d3b90d8d2ec78e99a94bb1489cf737f31b0c
Mr-Hadez <hwest1507@gmail.com>
2024-09-27 09:35:18 +1000

    Add random score generator to score status program

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,10 +3,16 @@ CP1404
 Amended a broken program to determine score status
 """
 
+import random
+
 
 def main():
     score = float(input("Enter score: "))
     result = determine_score_status(score)
+    print(f"Your score is {result}")
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score}")
+    result = determine_score_status(random_score)
     print(f"Your score is {result}")
 
 
