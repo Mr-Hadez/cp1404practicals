d000109c02b2cdbcec8969f386bbf2420085320e
Mr-Hadez <hwest1507@gmail.com>
2024-09-27 09:58:56 +1000

    Refactor score status program to be more efficient

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -5,11 +5,13 @@ Amended a broken program to determine score status
 
 score = float(input("Enter score: "))
 
-if 100 >= score >= 90:
-    print("Excellent")
-elif 90 > score >= 50:
-    print("Passable")
-elif 0 <= score < 50:
-    print("Bad")
-else:
+if score < 0 or score > 100:
     print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
+
+
