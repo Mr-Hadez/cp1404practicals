5f5e858862442a557f3404f6b771b8b2bdb37959
Mr-Hadez <hwest1507@gmail.com>
2024-09-27 22:02:41 +1000

    Refactor password checking to a seperate function and add docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,24 +1,28 @@
 """
-Checks password
+Checks password and prints stars for each letter in the password
 """
 
 
 def main():
+    """Get password, check for valid length and print stars using functions."""
     password = get_password()
+    print_stars(password)
+
+
+# Moved all password checking to function after seeing solution
+def get_password():
+    """Get password and check for valid length"""
+    password = input("Enter a password: ")
     minimum_length = 5
     while len(password) < minimum_length:
         print("Invalid input length!")
         password = get_password()
-    print_stars(password)
+    return password
 
 
 def print_stars(password):
+    """Print stars for each letter in password."""
     print("*" * len(password))
-
-
-def get_password():
-    password = input("Enter a password: ")
-    return password
 
 
 main()
