952f19d5536f983930fe8948ce28bd2081d015b4
Mr-Hadez <hwest1507@gmail.com>
2024-09-27 10:32:18 +1000

    Add star print for score

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -12,10 +12,12 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            score = float(input("Enter score: "))
+            score = int(input("Enter score: "))
         elif choice == "P":
             result = determine_score_status(score)
             print(f"{result}")
+        elif choice == "S":
+            print("*" * score)
         else:
             print("Invalid choice")
         print(MENU_STRING)
