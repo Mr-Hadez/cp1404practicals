6172813d0a6ead308f6a3384bda0b4d992abf8e9
Mr-Hadez <hwest1507@gmail.com>
2024-09-27 10:26:50 +1000

    Add a score menu program

---
+++ prac_02/score_menu.py
@@ -0,0 +1,26 @@
+"""
+Print stars for score
+"""
+
+from score import determine_score_status
+
+MENU_STRING = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
+
+
+def main():
+    print(MENU_STRING)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = float(input("Enter score: "))
+        elif choice == "P":
+            result = determine_score_status(score)
+            print(f"{result}")
+        else:
+            print("Invalid choice")
+        print(MENU_STRING)
+        choice = input(">>> ").upper()
+    print("Farewell")
+
+
+main()
