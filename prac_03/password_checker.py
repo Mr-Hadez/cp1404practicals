8cc9e4166e62be01260e27e7aec93f8819d4de22
Mr-Hadez <hwest1507@gmail.com>
2024-10-06 12:53:39 +1000

    Refactor password settings for testing

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -4,8 +4,8 @@ Password checker "skeleton" code to help you get started
 """
 
 MIN_LENGTH = 2
-MAX_LENGTH = 6
-IS_SPECIAL_CHARACTER_REQUIRED = False
+MAX_LENGTH = 10
+IS_SPECIAL_CHARACTER_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -30,9 +30,9 @@ def is_valid_password(password):
     if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
 
+    number_of_digit = 0
     number_of_lower = 0
     number_of_upper = 0
-    number_of_digit = 0
     number_of_special = 0
     for character in password:
         if character.isdigit():
@@ -44,7 +44,7 @@ def is_valid_password(password):
         if character in SPECIAL_CHARACTERS:
             number_of_special += 1
 
-    if number_of_digit == 0 or number_of_upper == 0 or number_of_lower == 0 or number_of_special == 0:
+    if number_of_digit == 0 or number_of_lower == 0 or number_of_upper == 0 or number_of_special == 0:
         return False
 
     # if we get here (without returning False), then the password must be valid
