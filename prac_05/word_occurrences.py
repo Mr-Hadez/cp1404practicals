22c4ba4e2959f8666d7139bee30c7d592e23660c
Mr-Hadez <hwest1507@gmail.com>
2024-10-18 10:04:34 +1000

    Add prac 5 word occurrences exercise

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,11 @@
+"""
+Word Occurrences
+Estimate: 15 minutes
+Actual:   32 minutes
+"""
+
+strings = input("Text: ").split(" ")
+word_to_occurrence = {string:strings.count(string) for string in strings}
+max_length = max(len(word) for word in (list(word_to_occurrence.keys())))
+for word in sorted(word_to_occurrence):
+    print(f"{word:{max_length}} : {word_to_occurrence[word]}")
