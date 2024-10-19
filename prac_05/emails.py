3a2d8b6d168239d3871c9cf5d6df03920132c621
Mr-Hadez <hwest1507@gmail.com>
2024-10-19 18:34:04 +1000

    Refactor to extract name in function for prac 5 emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,24 +3,28 @@ Email
 Estimate: 20 minutes
 Actual:   40 minutes
 """
+
+
 def main():
+    """Get email and extract name and store in dictionary."""
     email_to_name = {}
     email = input('Email: ')
     while email != "":
-        first_name = (email.split('@')[0]).split('.')[0]
-        if '.' in email.split('@')[0]:
-            last_name = (email.split('@')[0]).split('.')[1]
-        else:
-            last_name = ''
-        email_to_name[email] = f"{first_name} {last_name}"
-        choice = input(f'Is your name {email_to_name[email]} (Y/n) ').upper()
-        if choice == 'Y':
-            email = input('Enter your email: ')
-        else:
+        name = extract_name(email)
+        choice = input(f'Is your name {name} (Y/n) ').upper()
+        if choice != 'Y' and choice != '':
             name = input('Name: ')
-            email_to_name[email] = name
-            email = input('Enter your email: ')
+        email_to_name[email] = name
+        email = input('Email: ')
     for email, name in sorted(email_to_name.items()):
         print(f"{name} ({email})")
+
+
+def extract_name(email):
+    """Split first part of email to extract full name."""
+    parts = (email.split('@')[0]).split('.')
+    name = " ".join(parts).title()
+    return name
+
 
 main()
