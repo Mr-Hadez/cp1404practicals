4462f420e6591590daae7539405e18504197b87a
Mr-Hadez <hwest1507@gmail.com>
2024-10-19 19:04:06 +1000

    Refactor max length calculation to be more efficient in prac 5 word occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,7 +5,7 @@ Actual:   20 minutes
 """
 
 strings = input("Text: ").split(" ")
-word_to_occurrence = {string:strings.count(string) for string in strings}
-max_length = max(len(word) for word in (list(word_to_occurrence.keys())))
+word_to_occurrence = {string: strings.count(string) for string in strings}
+max_length = max(len(word) for word in word_to_occurrence)
 for word in sorted(word_to_occurrence):
     print(f"{word:{max_length}} : {word_to_occurrence[word]}")
