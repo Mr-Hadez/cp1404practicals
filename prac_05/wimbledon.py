46daabf0ce345c0490a948a5b5e6754c1c9a6649
Mr-Hadez <hwest1507@gmail.com>
2024-10-19 17:41:57 +1000

    Refactor to seperate prac 5 wimbledon program into different functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,13 +7,31 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
+    """Get data, extract champion information and display data."""
+    data = get_data()
+    champion_to_count, countries = extract_data(data)
+    display_data(champion_to_count, countries)
+
+
+def get_data():
+    """Read wimbledon file and return data."""
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
         next(reader)
         data = list(reader)
+    return data
+
+
+def extract_data(data):
+    """Extract countries in a set and champions and count to a dictionary."""
     countries = set([row[1] for row in data])
     champions = [row[2] for row in data]
     champion_to_count = {champion: champions.count(champion) for champion in champions}
+    return champion_to_count, countries
+
+
+def display_data(champion_to_count, countries):
+    """Print champions and number of times they have won and all the champion countries."""
     print("Wimbledon Champions: ")
     for champion, count in champion_to_count.items():
         print(champion, count)
