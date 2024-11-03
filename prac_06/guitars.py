4af29d744443332fde541048e6bbdc52db3d9a8d
Mr-Hadez <hwest1507@gmail.com>
2024-11-03 12:34:33 +1000

    Update output to match what is expected for the guitar playing program in prac 6

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -17,7 +17,8 @@ while name != "":
     print(f"{guitar} added.")
     name = input("Name: ")
 
+print("These are my guitars: ")
 for i, guitar in enumerate(guitars, 1):
-    guitar_age = guitar.get_age(2022)
-    vintage_string = "(vintage)" if guitar.is_vintage(guitar_age) else ""
+    guitar_age = guitar.get_age()
+    vintage_string = "(vintage)" if guitar.is_vintage() else ""
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
