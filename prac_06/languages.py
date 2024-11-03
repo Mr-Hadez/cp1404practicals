cd789c83a2777e1776de64b359b971091adb9f0f
Mr-Hadez <hwest1507@gmail.com>
2024-11-03 12:41:42 +1000

    Replace list comprehension with if statement for efficiency in the languages program in prac 6

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -9,10 +9,11 @@ from programming_language import ProgrammingLanguage
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
 print(python)
-languages = [ProgrammingLanguage("Python", "Dynamic", True, 1991), ProgrammingLanguage("Ruby", "Dynamic", True, 1995), ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
-print(languages)
-dynamic_languages = [language for language in languages if language.is_dynamic()]
+languages = [python, ruby, visual_basic]
+
 print("The dynamically typed languages are: ")
-for language in dynamic_languages:
-    print(language.name)
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
