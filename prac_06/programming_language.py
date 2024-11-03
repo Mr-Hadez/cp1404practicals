10e06f198cb32c5630d5032e7ff19410d6dc107b
Mr-Hadez <hwest1507@gmail.com>
2024-11-03 12:54:02 +1000

    Remove unnecessary method in programming languages class in prac 6

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,21 +1,20 @@
 """Programming languages class"""
 
+
 class ProgrammingLanguage:
     """Programming languages class"""
 
     def __init__(self, name="", typing="Static", reflection=False, year=0):
-        """Initialise programming languages instance"""
+        """Initialise programming languages instance."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
+        """Return a string view of the programming language."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
-    def __repr__(self):
-        return str(self)
-
     def is_dynamic(self):
-        """Determine if programming language is dynamic"""
+        """Determine if programming language is dynamic."""
         return self.typing == "Dynamic"
