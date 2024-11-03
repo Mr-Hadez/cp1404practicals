642981beaecc245432f15974522d393902a4c490
Mr-Hadez <hwest1507@gmail.com>
2024-11-03 12:13:58 +1000

    Refactor function calls to not take in unneeded arguments in the guitar testing program in prac 6

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,14 +1,15 @@
-"""Guitar testing program"""
+"""
+Guitar testing program
+Estimate: 10 minutes
+Actual:   10 minutes
+"""
 
 from guitar import Guitar
 
 guitar = Guitar("Gibson L-5 CES", 1922, 16035)
 another_guitar = Guitar("Another Guitar", 2013, 16035)
-guitar_age = guitar.get_age(2022)
-another_guitar_age = another_guitar.get_age(2022)
-is_guitar_vintage = guitar.is_vintage(guitar_age)
-is_another_guitar_vintage = guitar.is_vintage(another_guitar_age)
-print(f"Gibson L-5 CES get_age() - Expected 100. Got {guitar_age}")
-print(f"Another Guitar get_age() - Expected 9. Got {another_guitar_age}")
-print(f"Gibson L-5 CES is_vintage() - Expected True. Got {is_guitar_vintage}")
-print(f"Another Guitar is_vintage() - Expected False. Got {is_another_guitar_vintage}")
+
+print(f"Gibson L-5 CES get_age() - Expected 100. Got {guitar.get_age()}")
+print(f"Another Guitar get_age() - Expected 9. Got {another_guitar.get_age()}")
+print(f"Gibson L-5 CES is_vintage() - Expected True. Got {guitar.is_vintage()}")
+print(f"Another Guitar is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
