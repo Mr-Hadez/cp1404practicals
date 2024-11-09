89baa1051e76bb318740b16134166f5535f39582
Mr-Hadez <hwest1507@gmail.com>
2024-11-09 23:30:25 +1000

    Add guitar class to prac 7

---
+++ prac_07/guitar.py
@@ -0,0 +1,30 @@
+"""
+Guitar class
+Estimate: 5 minutes
+Actual:   5 minutes
+"""
+
+VINTAGE_AGE = 50
+CURRENT_YEAR = 2022
+
+
+class Guitar:
+    """Guitar class"""
+
+    def __init__(self, name="", year=0, cost=0.0):
+        """Initialise Guitar constructor."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string view of Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):
+        """Calculate the age of the guitar."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if the guitar is vintage."""
+        return self.get_age() >= VINTAGE_AGE
