c7a5d7641e181d580ffda8c81304d1dd109f8f62
Mr-Hadez <hwest1507@gmail.com>
2024-11-23 20:39:25 +1000

    Add print statement for distance driven for unreliable car test in prac 9

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,7 +1,8 @@
 """Unreliable car test"""
 from prac_09.unreliable_car import UnreliableCar
 
-my_car = UnreliableCar("Prius 1",100, 65)
-for i in range(1,11):
-    my_car.drive(10)
+my_car = UnreliableCar("Prius 1", 100, 65)
+for i in range(10):
+    distance_driven = my_car.drive(10)
+    print(distance_driven)
 print(my_car)
