8cf30b151964b019e823435a797611eafb3c4b0a
Mr-Hadez <hwest1507@gmail.com>
2024-11-29 11:16:57 +1000

    Uncomment doctest line in prac 10

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -41,11 +41,11 @@ def run_tests():
     car = Car()
     assert car.fuel == 0, "Car does not set fuel correctly"
 
+
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
 # TODO: 4. Fix the failing is_long_word function
 # (Don't change the tests, change the function!)
