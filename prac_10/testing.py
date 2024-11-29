eb558b67b24df91223ca59367b9d4fd4bbbe0a81
Mr-Hadez <hwest1507@gmail.com>
2024-11-29 11:13:34 +1000

    Include assert statements to test setting car fuel in prac 10

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -32,15 +32,14 @@ def run_tests():
     """Run the tests on the functions."""
     assert repeat_string("Python", 1) == "Python"
     assert repeat_string("hi", 2) == "hi hi"
+
     car = Car()
     assert car._odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
     car = Car(fuel=10)
-
+    assert car.fuel == 10, "Car does not set fuel correctly"
+    car = Car()
+    assert car.fuel == 0, "Car does not set fuel correctly"
 
 run_tests()
 
