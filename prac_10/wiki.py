9e102992a652cd767c25d6715fdaa09a2e2bdcbd
Mr-Hadez <hwest1507@gmail.com>
2024-12-01 18:33:37 +1000

    Add error checking to wiki program in prac 10

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -10,17 +10,25 @@ def main():
     """Prompts user for page title and print some details of the page, ends when user enters blank input."""
     title = input("Enter page title: ")
     while title != "":
-        print_page_details(title)
+        display_page_details(title)
         title = input("Enter page title: ")
     print("Thank you.")
 
 
-def print_page_details(title):
+def display_page_details(title):
     """Print page title, short summary and URL."""
-    page = wikipedia.page(title)
-    print(page.title)
-    print(wikipedia.summary(title, 4))  # Truncate output to match sample output
-    print(page.url)
+    try:
+        page = wikipedia.page(title, auto_suggest=False)
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("We need a more specific title. Try one of the following, or a new search:")
+        print(wikipedia.suggest(title))
+        print(e.options)
+    except wikipedia.exceptions.PageError:
+        print(f'Page id "{title}" does not match any pages. Try another id!')
+    else:
+        print(page.title)
+        print(page.summary)
+        print(page.url)
 
 
 if __name__ == '__main__':
